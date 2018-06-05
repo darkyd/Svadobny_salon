ea826d8f3a784f1c04cc1eb48b93698ec02fc464
Dávid Majerčík <david.majercik@gmail.com>
2018-06-05 17:12:49 +0200

    databaza uprava

--- svadobny_salon/databaze.php
+++ svadobny_salon/databaze.php
@@ -1,7 +1,7 @@
-﻿<?php
+<?php
     $hostname='localhost';
     $username='root';
-    $password='';
+    $password='password';
     $dbname='sarah';
     $conn = mysqli_connect($hostname, $username, $password, $dbname);
     if (!$conn) {
