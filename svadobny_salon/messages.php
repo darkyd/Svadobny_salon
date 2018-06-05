d9facbb27f1f91edb05cf3b9913c9b61a75b1821
Dávid Majerčík <david.majercik@gmail.com>
2018-06-05 20:55:50 +0200

    finally project

--- svadobny_salon/messages.php
+++ svadobny_salon/messages.php
@@ -1,7 +1,7 @@
 <?php
 include('databaza.php'); // Includuje údaje pre pripojenie k DB
 
-$vyber = mysqli_query($conn,"SELECT * FROM `chat` ORDER BY `cas` DESC"); // Vybere z tabulky chat, zoradené podľa času
+$vyber = mysqli_query($conn,"SELECT * FROM `chat` ORDER BY `cas` DESC"); // Vyberie z tabuľky chat, zoradené podľa času
 while($riadok = $vyber->fetch_assoc()) // Pokiaľ nie je na konci, vyberá správy
 {
 echo htmlspecialchars($riadok['cas'] . " ~ " . $riadok['meno'] . " ~ " . $riadok['sprava']); echo("<br><br>"); // Vypíše správy, htmlspecialchars zakazuje HTML
--- svadobny_salon/msg_write.php
+++ svadobny_salon/msg_write.php
@@ -1,17 +1,17 @@
 <?php
-session_start(); // Slouží pre uloženie mena
+session_start(); // Služí pre uloženie mena
 include('databaza.php'); // includuje soubor s udajmi do databázy
 $meno = mysqli_real_escape_string($conn,$_POST['meno']); // Ochrana proti SQL injekci
 $sprava = mysqli_real_escape_string($conn,$_POST['sprava']); 
 
 if($meno != "" || $sprava != "") // Ak meno a správa niečo obsahujú (nesú prázdne)
 {
-mysqli_query($conn,"INSERT INTO `chat`(`meno`, `sprava`,`cas`) VALUES ('$meno','$sprava',Now())"); // Vloží do databázy... vkládame iba do meno, správa a dátum, ID se vloží samo
+mysqli_query($conn,"INSERT INTO `chat`(`meno`, `sprava`,`cas`) VALUES ('$meno','$sprava',Now())"); // Vloží do databázy... vkládame iba do meno, správa a dátum, ID sa vloží samo
 $_SESSION['meno'] = $meno; // Nastaví meno
-header("Location: chat_type.php"); // Vrátí sa späť k pisaniu správy
+header("Location: chat_type.php"); // Vráti sa späť k pisaniu správy
 }
 else // ak je niečo prázdne
 {
-header("Location: chat_type.php"); // Vrátí sa späť k pisaniu správy
+header("Location: chat_type.php"); // Vráti sa späť k písaniu správy
 }
 ?>
