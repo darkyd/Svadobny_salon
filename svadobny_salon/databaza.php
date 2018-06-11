78e6034f54bce6c05fd6241dcdd7be612833b9c2
darkyd <38666415+darkyd@users.noreply.github.com>
2018-06-11 20:06:41 +0200

    Update databaza.php

--- svadobny_salon/databaza.php
+++ svadobny_salon/databaza.php
@@ -1,7 +1,7 @@
 <?php
 $hostname='localhost';
 $username='root';
-$password='';
+$password='password';
 $dbname='sarah';
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if (!$conn) {
